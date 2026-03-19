95bee9db735b39ed7a346cc32c77d7184214412a
huyoo <juliannekurtt@gmail.com>
2026-03-19 22:30:32 +0800

    yung main nalang

--- SchoolEventRegistration/TicketingSystem/src/TicketingSystemMain.java
+++ SchoolEventRegistration/TicketingSystem/src/TicketingSystemMain.java
@@ -1,13 +1,9 @@
-import java.util.*;
+// import java.util.*;
 
 public class TicketingSystemMain {
-
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in); 
-
     
-    Event e1 = new Event("try lang", "kurt event", "samin", "december 11 2005", 50, 10);
-   System.out.println(e1.getEventId());
+        
 
 
     }
